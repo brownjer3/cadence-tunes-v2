70d0cd8551f376e71e8f6333485453c6eac2aed8
jerry <brownjer3@gmail.com>
2021-02-15 19:18:36 -0600

    transform genre selection into collection of radio inputs

--- frontend/index.js
+++ frontend/index.js
@@ -14,16 +14,31 @@ const topList = document.getElementById('topList')
 
 document.addEventListener("DOMContentLoaded", () => {
     loadTopPlaylists()
+    loadGenres()
 })
 
 function loadTopPlaylists() {
     for (let i=0; i<10; i++) {
-        const liItem = document.createElement('li')
-        loadPlaylistLi(liItem)
+        const liItem = document.createElement('div')
+        liItem.innerHTML = `<li id='playlist-${i}'> Playlist ${i+1} </li>`
         topList.appendChild(liItem)
     }
 }
 
-function loadPlaylistLi(item) {
-    item.innerHTML = `<li> hi there </li>`
+// function loadPlaylistLi(item) {
+//     item.innerHTML = `<li> Song # </li>`
+// }
+
+
+// load genre options
+function loadGenres() {
+    const genreOptions = document.getElementById('genres')
+    for (let i=0; i<20; i++) {
+        const input = document.createElement('div')
+        input.classList.add("col-2", "p-3")
+        input.innerHTML = `<input id='genre-${i}' type="radio" class="btn-check" name="genre" value='genre-${i}'>
+            <label class="btn btn-primary" for='genre-${i}'>Genre ${i+1}</label>
+        `
+        genreOptions.appendChild(input)
+    }
 }
