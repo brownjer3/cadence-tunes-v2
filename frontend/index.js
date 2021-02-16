d9468fb4d5bbf5e886fcbb32dfcaef3118586d23
jerry <brownjer3@gmail.com>
2021-02-15 18:29:02 -0600

    add event listeners to builder submit button and top playlists list

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,20 @@
-const hello = document.getElementById('title')
+// submit form button logic
+const submit = document.getElementById('submit')
 
-hello.addEventListener("click", (e) => {
-    e.target.innerText = 'Goodbye world'
+submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    //check that the fields have content
+    //submit fetch request
 })
+
+
+
+//populate top playlists list
+const topList = document.getElementById('topList')
+
+document.addEventListener("DOMContentLoaded", () => {
+    const liItem = document.createElement('li')
+    liItem.innerHTML = `<li> hi there </li>`
+    topList.appendChild(liItem)
+})
+
