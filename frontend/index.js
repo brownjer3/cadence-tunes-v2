b8efef4ee3449426adb9648d3aa045bcbc1eedd2
jerry <brownjer3@gmail.com>
2021-02-17 15:50:21 -0600

    restructure files for .env access

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,10 @@
 //this top section should stay here in index.js
 const port = "http://localhost:3000"
 const playlistApi = new PlaylistApi(port)
+const spotifyApi = new SpotifyApi()
 
 
-// submit form button logic
+// submit form button
 const submit = document.getElementById('submit')
 
 submit.addEventListener('click', (e) => {
@@ -12,5 +13,5 @@ submit.addEventListener('click', (e) => {
     //submit fetch request
 })
 
-
 playlistApi.getPlaylists()
+// spotifyApi.authenticate()
