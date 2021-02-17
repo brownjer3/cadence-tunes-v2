5c6396b275c4fea74a699d4e249d9b542b8a972c
jerry <brownjer3@gmail.com>
2021-02-17 16:18:35 -0600

    relocate spotify authentication to the backend

--- frontend/src/spotifyApi.js
+++ frontend/src/spotifyApi.js
@@ -6,26 +6,11 @@
 class SpotifyApi {
 
     constructor() {
-        this.apiTokenUrl = "https://accounts.spotify.com/api/token"
-        this.genreUrl = "https://api.spotify.com/v1/recommendations/available-genre-seeds"
+
     }
 
     authenticate() {
-        // const encoded = btoa(`${ENV["SPOTIFY_CLIENT_ID"]}:${ENV["SPOTIFY_CLIENT_SECRET"]}`)
-        const encoded = "NzIyMWRiODEyZDY5NDhjZThjNTk2MThmODdiYjUyY2E6MDg4YjkxODk2OTYyNDRmZDg2YTIwODU2YzQ5MTk5YmQ="
 
-        fetch(this.apiTokenUrl, {
-            method: "POST",
-            headers: {
-                Authorization: `Basic ${encoded}`
-            }, 
-            body: {
-                content_type: "application/x-www-form-urlencoded",
-                grant_type: "client_credentials"
-            }
-        })
-        .then(r => r.json())
-        .then(json => console.log(json)) 
     }
 
     static fetchGenres() {
