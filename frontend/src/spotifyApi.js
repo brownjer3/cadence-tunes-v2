dc911ebf4794cfd9411e7d5a0b09628d1dcbd166
jerry <brownjer3@gmail.com>
2021-02-19 19:59:38 -0600

    spotify recs cadence bug fix

--- frontend/src/spotifyApi.js
+++ frontend/src/spotifyApi.js
@@ -34,7 +34,7 @@ class SpotifyApi {
 
     getRecs() {
         const genre = document.querySelector('input[name="genre"]:checked').value
-        const cadence = document.getElementById('cadence').value
+        const cadence = parseInt(document.getElementById('cadence')).value
         const data = {
             genre: genre,
             cadence: cadence
